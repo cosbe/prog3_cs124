bea16538a6ea51fb0e42746af73a49ec2d0da04a
Derek Zheng <derek_zheng@college.harvard.edu>
2020-04-24 16:10:48 -0700

    update

--- partition.cpp
+++ partition.cpp
@@ -1,14 +1,38 @@
 // my first program in C++
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main()
+
+int[] A; 
+
+int main(int argc, char *argv[])
 {
-    int flag = -1; 
-    std::cin >> flag;
+    int flag = atoi(argv[1]); 
 
-    int algorithm = -1; 
-    std::cin >> algorithm;
+    int algorithm = atoi(argv[2]); 
+
+    string fileName = argv[3];
+    ifstream inputFile;
+    inputFile.open(fileName);
 
     
-  std::cout << "Hello World!";
+    inputFile.close();
+  std::cout << "Hello World!" << " " << flag << " " << algorithm << " " << fileName ;
 }
+
+//runs karmarkarkarp on the algorithm
+int[] karmarkarKarp(vector<int> p) {
+    void; 
+}
+
+int[] repeated_random(int[] part) {
+    void;
+}
+
+int[] hill_climbing(int[] part) {
+    void;
+}
+
