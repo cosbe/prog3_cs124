d3796dbdc971de0584152d9adf482586d02040c9
Derek Zheng <derek_zheng@college.harvard.edu>
2020-04-24 15:16:19 -0700

    init prog.

---
+++ partition.cpp
@@ -0,0 +1,14 @@
+// my first program in C++
+#include <iostream>
+
+int main()
+{
+    int flag = -1; 
+    std::cin >> flag;
+
+    int algorithm = -1; 
+    std::cin >> algorithm;
+
+    
+  std::cout << "Hello World!";
+}
