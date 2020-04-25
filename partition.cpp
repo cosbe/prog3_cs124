63fab2b69fa4414e554e861b27ab8d76a46ff5bc
Derek Zheng <derek_zheng@college.harvard.edu>
2020-04-24 17:00:33 -0700

    working iostream

--- partition.cpp
+++ partition.cpp
@@ -15,20 +15,29 @@ int main(int argc, char *argv[])
     int algorithm = atoi(argv[2]);
 
     string fileName = argv[3];
-    ifstream inputFile;
-    inputFile.open(fileName);
+    std::ifstream file(fileName);
        // store into A
-    inputFile.close();
+    string k; 
+    while (std::getline(file, k)){
+        int i = stoi(k);
+        A.push_back(i);
+    }
+    file.close();
 
 //   given file, read from it to get list L of 100 ints.
 
-//   create initial random P (generate sequence of n values from range [1,n])
+// n values from range [1,n])
 
 //   // Record amount of time it takes for each algorithm
 //   kar_karp (sorted L)
 //   repeated_random P
 //   hill_climbing P
 //   sim_anneal P
+
+    for(int i = 0; i < A.size(); i++) {
+        printf(" %d ", A[i]);
+    }
+
 
   std::cout << "Hello World!";
 }
@@ -40,13 +49,23 @@ vector<int> random_P(int n) {
 
 // Runs karmarkarkarp on the algorithm
 vector<int> karmarkarKarp(vector<int> p) {
-  // create A' based on P and A
-  // sort A'
-  // pseudo for kar_karp
+    // create A' based on P and A
+    vector<int> ap(p.size(), 0);
+    for(int i = 0; i < p.size();  i++) {
+        ap[p[i]]+=A[i];
+    }
+
+    // sort A'
+    sort(ap.begin(), ap.end(), greater<int>());
+    for(int i = 0; i < ap.size(); i++) {
+        printf(" %d ", ap[i]);
+    }
+
+    // pseudo for kar_karp
     void;
 }
 
-vector<int> repeated_random(int[] part) {
+vector<int> repeated_random(vector<int> p) {
   // for iter 1 to 25,000
   // S' = random_P
   // if kar_karp S' < kar_karp S then S = S'
@@ -54,11 +73,11 @@ vector<int> repeated_random(int[] part) {
     void;
 }
 
-vector<int> hill_climbing(int[] part) {
+vector<int> hill_climbing(vector<int> p) {
     void;
 }
 
-vector<int> sim_anneal(int[] part) {
+vector<int> sim_anneal(vector<int> p) {
     void;
 }
 
